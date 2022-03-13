668d208b853145e08e22d0f416f3cced46c75a09
geunheejung <geunhee0212@gmail.com>
2022-03-13 09:30:01 +0900

    search-form-3 구현.

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -18,15 +18,24 @@ export default class SearchFormView extends View {
 
   bindEvent = () => {
     on(this.inputElement, 'keyup', () => this.handleKeyUp());
+    this.on('submit', event => this.handleSubmit(event));
   }
 
   handleKeyUp = () => {
     const { value } = this.inputElement;
     const { style } = this.resetElement;
-    // value의 길이가 0 -> 1 또는 1 -> 0 으로 바뀌는 순간에만
-    // none인 상태이면서 value length가 존재할 경우
+
     if (style.display === 'none' && value.length > 0) this.showResetButton();
     if (!value.length) this.showResetButton(false);
+  }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { value } = this.inputElement;
+    const payload = {
+      value
+    }
+
+    this.emit(`@submit`, payload);
   }
 }
