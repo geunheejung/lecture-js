1ad228becb3bfea7cfa6e624f75c42353a775969
geunheejung <geunhee0212@gmail.com>
2022-03-21 16:05:32 +0900

    헤더 구현.

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,2 +1,7 @@
-const element = <h1>Hello world</h1>
+const element = (
+  <header>
+    <h2 className="container">검색</h2>
+  </header>
+) // js 표현식이기에 차이가 있다. -> 속성 이름이 다르다. jsx는 카멜 케이스를 사용한다.
+
 ReactDOM.render(element, document.querySelector("#app"))
